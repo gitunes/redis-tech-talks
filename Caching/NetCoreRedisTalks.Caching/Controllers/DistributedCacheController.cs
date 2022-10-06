17a73f71ed6a9f3ad9387896b95f5c76df38bd37
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-10-06 23:38:29 +0300

    Add sample methods for distributed cache

---
+++ Caching/NetCoreRedisTalks.Caching/Controllers/DistributedCacheController.cs
@@ -0,0 +1,59 @@
+﻿namespace NetCoreRedisTalks.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DistributedCacheController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public DistributedCacheController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public IActionResult GetString()
+        {
+            string cachedValue = _distributedCache.GetString("vehicles-distributed-cache");
+            if (string.IsNullOrEmpty(cachedValue))
+                return NotFound("vehicle not found");
+
+            var vehicles = JsonSerializer.Deserialize<List<Vehicle>>(cachedValue);
+            return Ok(vehicles);
+        }
+
+        [HttpPost]
+        public IActionResult SetString([FromBody] List<Vehicle> vehicles)
+        {
+            DistributedCacheEntryOptions distributedCacheEntryOptions = new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(10),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            };
+
+            var serilazedValue = JsonSerializer.Serialize(vehicles);
+
+            _distributedCache.SetString("vehicles-distributed-cache", serilazedValue, distributedCacheEntryOptions);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult SetFile()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/volkswagen.jpg");
+            byte[] imageByte = System.IO.File.ReadAllBytes(path);
+
+            _distributedCache.Set("volkswagen-distributed-cache", imageByte);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("vehicles-distributed-cache");
+            return Ok();
+        }
+    }
+}
--- Caching/NetCoreRedisTalks.Caching/Usings.cs
+++ Caching/NetCoreRedisTalks.Caching/Usings.cs
@@ -1,4 +1,6 @@
 ﻿global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Caching.Distributed;
 global using Microsoft.Extensions.Caching.Memory;
 global using NetCoreRedisTalks.Caching.Infrastructure;
 global using NetCoreRedisTalks.Caching.Models;
+global using System.Text.Json;
